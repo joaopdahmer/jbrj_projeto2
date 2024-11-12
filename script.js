fd94d9b766c40aa7af0f7dd47dc1fe95572fcd4c
João Pedro <joaopdahmer@gmail.com>
2024-11-12 10:07:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -43,12 +43,12 @@ function mostrarPlantaImagem(planta) {
 
     const imageUrl = {
         'camellia': 'https://upload.wikimedia.org/wikipedia/commons/e/e6/Camellia_sinensis_drawing.jpg',
-        'tectona': 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Starr_010304-0485_Tectona_grandis.jpg/1024px-Starr_010304-0485_Tectona_grandis.jpg',
+        'tectona': 'https://cloud.jbrj.gov.br/apps/files_sharing/publicpreview/TdLyQ7ZzkCBCmDK?file=/&fileId=18006061&x=1441&y=761&a=true&etag=88830e5191336174b8701e85c340ffa7',
         'artocarpus': 'https://cloud.jbrj.gov.br/apps/files_sharing/publicpreview/6FSkFpeJcrN8npF?file=/&fileId=17944112&x=1927&y=920&a=true&etag=b086dc8c875719c27b005e285a81f023',
-        'stifftia': 'https://cloud.jbrj.gov.br/apps/files_sharing/publicpreview/SJEBgty3Zspf2WK?file=/&fileId=17955227&x=1927&y=920&a=true&etag=0b6a4b856343f59f459f79b898da422e',
+        'stifftia': 'https://cloud.jbrj.gov.br/apps/files_sharing/publicpreview/oHqr4SyxeebiBQM?file=/&fileId=18006063&x=1441&y=761&a=true&etag=bec5f260a05420222e02cc202bbc0355',
         'annona': 'https://cloud.jbrj.gov.br/apps/files_sharing/publicpreview/pBdB65BDrQPeBHX?file=/&fileId=17944122&x=1927&y=920&a=true&etag=096e795f612361b3ff19f0bc417a85bd',
-        'cenostigma': 'https://cloud.jbrj.gov.br/apps/files_sharing/publicpreview/JkRCiBY5BGmXbar?file=/&fileId=17946713&x=1927&y=920&a=true&etag=23eab4b83e6242cb0615d35154f39796',
-        'persea': 'https://cloud.jbrj.gov.br/apps/files_sharing/publicpreview/3Wf4Q3Kj4RWDgEs?file=/&fileId=17946734&x=1927&y=920&a=true&etag=0813af235bcf5f414f6154895af48f7a',
+        'cenostigma': 'https://cloud.jbrj.gov.br/apps/files_sharing/publicpreview/cz6Z7zDSSKD47AN?file=/&fileId=18006062&x=1441&y=761&a=true&etag=b90cb9b6b856db4b82583932fc602d20',
+        'persea': 'https://cloud.jbrj.gov.br/apps/files_sharing/publicpreview/tSyq2oD8LPj62kZ?file=/&fileId=18006060&x=1441&y=761&a=true&etag=f166e7f4020077de3b04e42cb7875e86',
         'albizia': 'https://cloud.jbrj.gov.br/apps/files_sharing/publicpreview/s8MjLATgZYYNwEL?file=/&fileId=17946774&x=1927&y=920&a=true&etag=7a36eef3361525dc41d6fa10d826ce9e',
         'dimocarpus': 'https://cloud.jbrj.gov.br/apps/files_sharing/publicpreview/QNskkTaRdamRAFT?file=/&fileId=17946810&x=1927&y=920&a=true&etag=6b21e8018fa5dda759e9d88e223c800d',
         'artocarpus2': 'https://cloud.jbrj.gov.br/apps/files_sharing/publicpreview/TDMYgQJGW7QirAo?file=/&fileId=17946839&x=1927&y=920&a=true&etag=109dd2b574c1d36a21c667334b562643'
